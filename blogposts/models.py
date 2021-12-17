cb40d8b3509d17b831ed5a44e5c825f4b0748bd1
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-12-17 12:34:48 -0800

    Created blogpost DB model + migrated to DB + registered with admin

--- blogposts/models.py
+++ blogposts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
