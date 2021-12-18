ac817d44721949c5f4ff4bce8d240a9d9e9fa77f
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-12-18 11:42:21 -0800

    no time to ignore sensitive info

---
+++ travelblogapi/constants/db_credentials.py
@@ -0,0 +1,7 @@
+name = "d699ipteehnuqb"
+host = "ec2-107-21-211-154.compute-1.amazonaws.com"
+port = 5432
+username = "vejhrnqktrbicz"
+password = "7cf4f0af9bebbdc4e1d990469445b6cf5c23dca83da779019959046e0963cd09"
+
+secret_key = "h)rv@q*5&2!!zvr5xa^opxn%ew!i$qaun57j#1pgvga!&dyl(h"
